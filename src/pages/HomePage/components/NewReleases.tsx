bffc8adc139ce093f22d735c6cf4b9b6337aeb4e
Yeonguk Bak <yeonguk0517@gmail.com>
2025-12-27 02:04:43 +0900

    주석 제거

--- src/pages/HomePage/components/NewReleases.tsx
+++ src/pages/HomePage/components/NewReleases.tsx
@@ -6,7 +6,6 @@ import Card from '../../../common/components/Card';
 
 const NewReleases = () => {
   const { data, error, isLoading } = useGetNewReleases();
-  console.log('🚀 ~ NewReleases ~ data:', data);
 
   if (isLoading) {
     return <LoadingSpinner />;
