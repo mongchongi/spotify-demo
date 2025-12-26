c496e8637557a422412db7682e1e6ffc3cc2f4a8
Yeonguk Bak <yeonguk0517@gmail.com>
2025-12-27 02:02:20 +0900

    authApi 코드 수정

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -3,11 +3,7 @@ import { clientId, clientSecretId } from '../configs/authConfig';
 import type { ClientCredentialTokenResponse } from '../models/auth';
 
 const encodedBase64 = (data: string): string => {
-  if (typeof window !== 'undefined') {
-    return btoa(data);
-  } else {
-    return Buffer.from(data).toString('base64');
-  }
+  return btoa(data);
 };
 
 export const getClientCredentialToken = async (): Promise<ClientCredentialTokenResponse> => {
