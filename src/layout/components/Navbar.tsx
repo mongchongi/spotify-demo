fb6ab0f8e50cb24fea274b40077069e50bdcd06b
Yeonguk Bak <yeonguk0517@gmail.com>
2025-12-27 13:20:56 +0900

    프로필 코드 수정

--- src/layout/components/Navbar.tsx
+++ src/layout/components/Navbar.tsx
@@ -1,14 +1,20 @@
 import { Box } from '@mui/material';
 import SignInButton from '../../common/components/SignInButton';
 import useGetCurrentUserProfile from '../../hooks/useGetCurrentUserProfile';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Navbar = () => {
   const { data: userProfile } = useGetCurrentUserProfile();
-  console.log('🚀 ~ Navbar ~ userProfile:', userProfile);
 
   return (
     <Box display={'flex'} alignItems={'center'} justifyContent={'flex-end'} height={'64px'}>
-      {userProfile ? <img src={userProfile.images[0]?.url} alt='' /> : <SignInButton />}
+      {!userProfile ? (
+        <SignInButton />
+      ) : userProfile.images[0].url ? (
+        <img src={userProfile.images[0].url} />
+      ) : (
+        <AccountCircleIcon />
+      )}
     </Box>
   );
 };
